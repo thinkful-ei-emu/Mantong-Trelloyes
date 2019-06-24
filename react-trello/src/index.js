4908cd29ae795d3af79b3694356f856669c62ce2
Mantong Zhao <zhaomantong0203@gmail.com>
2019-06-24 14:21:13 -0700

    solution

--- react-trello/src/index.js
+++ react-trello/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import STORE from './STORE'
 import App from './App';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
