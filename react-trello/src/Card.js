d17cffccd0d07514944f5bf28be2bd8a0cd758b4
Mantong Zhao <zhaomantong0203@gmail.com>
2019-06-24 14:14:56 -0700

    final update

--- react-trello/src/Card.js
+++ react-trello/src/Card.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import STORE from './store';
-function Card(props){
-  
-    return(
+import './Card.css';
 
-    <div className={`Card`}>
-        <h3> {props.card.title} </h3>
-        <p> {props.card.content} </p>
-        
+export default function Card(props) {
+  return (
+    <div className='Card'>
+      <button
+        type='button'
+      >
+        delete
+      </button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
     </div>
-   )
+  )
 }
-export default Card;
-/* <div class="Card">
-            <button type="button">delete</button>
-            <h3>Eleventh card</h3>
-            <p>lorem ipsum</p>
-</div> */
